1655b847d9b81f74de7d8c2cd62ecf4c1aba52db
KacperParzych <175176@student.uwm.edu.pl>
2024-03-24 08:56:17 +0100

    1.0

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import np
 import numpy as np
 a = np.array([0, 1])
 print(a)
@@ -10,7 +11,58 @@ print(a.dtype)
 
 a = np.arange(2, dtype='int32')
 print(a.dtype)
+
+print("\n\n")
 b = a.astype('float')
 print(b)
 print(b.dtype)
 print(b.shape)
+
+print("\n\n")
+m = np.array((np.arange(2), np.arange(2)))
+print(m)
+print(m.shape)
+print(m.ndim)
+
+print("\n\n")
+macierz = np.array([[1, 2], [3, 4], [5, 6]])
+print(macierz)
+print(macierz.shape)
+print(macierz.ndim)
+
+print("\n\n")
+zera = np.zeros((5, 5))
+jedynki = np.ones((4, 4))
+print(zera)
+print("\n\n")
+print(jedynki)
+print(zera.dtype)
+print(jedynki.dtype)
+
+print("\n\n")
+pusta = np.empty((2, 2))
+print(pusta)
+poz_1 = pusta[1, 1]
+poz_2= pusta[0, 1]
+print(poz_1)
+print(poz_2)
+
+print("\n\n")
+macierz = np.array([[1, 2], [3, 4]])
+print(macierz)
+liczby = np.arange(1, 2, 0.1)
+print(liczby)
+
+print("\n\n")
+liczby_lin = np.linspace(1, 2 , 5, endpoint=False)
+print(liczby_lin)
+
+print("\n\n")
+z = np.indices((5, 3))
+print(z)
+
+print("\n\n")
+x, y = np.mgrid[0:5, 0:5]
+print(x, "\n")
+print(y)
+
