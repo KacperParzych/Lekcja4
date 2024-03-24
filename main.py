068986482e4a4a864e311fe6ff903774a4fd82c9
KacperParzych <175176@student.uwm.edu.pl>
2024-03-24 08:33:02 +0100

    1.0

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+a = np.array([0, 1])
+print(a)
+
+a = np.arange(2)
+print(a)
+print(type(a))
+
+print(a.dtype)
+
+a = np.arange(2, dtype='int32')
+print(a.dtype)
+b = a.astype('float')
+print(b)
+print(b.dtype)
+print(b.shape)
