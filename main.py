2cd5ca004881b52e05854289eda556773a152438
KacperParzych <175176@student.uwm.edu.pl>
2024-03-24 09:38:40 +0100

    1.0

--- main.py
+++ main.py
@@ -71,9 +71,53 @@ print(z[0, 0, 1])
 print(z.ndim)
 
 print("\n\n")
-mat_diag_k = np.diag([a for a in range(5)])
+mat_diag_k = np.diag([a for a in range(7)], k=1)
 print(mat_diag_k)
 
 print("\n\n")
 z = np.fromiter(range(5), dtype = 'int32')
 print(z)
+
+print("\n\n")
+znaki = b'abcdef'
+zn1 = np.frombuffer(znaki, dtype = 'S1')
+print(zn1)
+print("\n\n")
+zn2 = np.frombuffer(znaki, dtype='S2')
+print(zn2, "\n")
+
+znaki = 'abcdef'
+zn3 = np.array(list(znaki))
+zn4 = np.array(list(znaki), dtype='S1')
+zn5 = np.array(list(b'abcdef'))
+zn6 = np.fromiter(znaki, dtype='S1')
+zn7 = np.fromiter(znaki, dtype='U1')
+print(zn3, "\n")
+print(zn4, "\n")
+print(zn5, "\n")
+print(zn6, "\n")
+print(zn7, "\n")
+
+mat = np.ones((2, 2))
+mat_1 = np.ones((2, 2))
+mat = mat + mat_1
+print(mat, "\n")
+print(mat - mat_1, "\n")
+print(mat*mat_1, "\n")
+print(mat/mat_1, "\n")
+
+
+a = np.dot(mat, mat_1)
+print(a, "\n")
+b = mat.dot(mat_1)
+print(b, "\n")
+
+a = np.arange(10)
+print(a, "\n")
+s = slice(2,7,2)
+print(a[s], "\n")
+s = range(2,7,2)
+print(a[s], "\n")
+print(a[2:7:2], "\n")
+print(a[1:], "\n")
+print(a[2:5], "\n")
